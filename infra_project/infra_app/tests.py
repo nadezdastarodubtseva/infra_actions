990c6ac8f4faa939acf2ce8ed05bca2c1c9915f7
Nadezda <nadezdastarodubtseva@yandex.ru>
2021-06-25 14:02:22 +0300

    change views

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -8,10 +8,10 @@ class StaticPagesURLTests(TestCase):
 
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
-        response = self.guest_client.get(reversed('index'))
+        response = self.guest_client.get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-        response = self.guest_client.get(reversed('second_page'))
+        response = self.guest_client.get('/second_page/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_context(self):
