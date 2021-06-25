c033b62987760e65cdf9010c25668630f5b9693a
Nadezda <nadezdastarodubtseva@yandex.ru>
2021-06-25 14:05:23 +0300

    change urls and views

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!', status=200)
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница', status=200)
+    return HttpResponse('А это вторая страница')
