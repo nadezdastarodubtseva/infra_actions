0e600dff1a12ae7a48bf47df454677dbda9011a8
Nadezda <nadezdastarodubtseva@yandex.ru>
2021-06-25 13:33:17 +0300

    changes

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'infra_app.apps.InfraAppConfig',
+    'infra_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
